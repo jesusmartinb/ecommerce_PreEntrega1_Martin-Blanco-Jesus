f0af501bd551431eb8db64e15eba87af01af9e41
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2023-06-27 18:52:29 +0200

    Se crea el componente ItemListContainer y se le pasa la prop greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Header from './components/Header'
+import ItemListContainer from './components/ItemListContainer'
 import NavBar from './components/NavBar'
 
 function App() {
+
+  const greeting = "Hello World, I'm learning React";
 
   return (
     <>
       <Header className="sticky top-0" />
       <NavBar />
+      <ItemListContainer greeting={greeting}/>
     </>
   )
 }
